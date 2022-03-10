7f156f09cd0073f96f51ca7ec4d4398a7e6fca40
Gabriel Schubert <schubertyi@gmail.com>
2022-03-10 04:14:20 -0300

    Moved BlocProvider to global scope

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo_app/home.dart';
+import 'package:todo_app/notes/bloc/notes_bloc.dart';
 import 'package:todo_app/repositories/notes_repository.dart';
 
 class TodoApp extends StatelessWidget {
@@ -10,10 +11,14 @@ class TodoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider(
       create: (context) => NoteRepository(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'To-Dos App',
-        home: HomePage(),
+      child: BlocProvider(
+        create: (context) => NotesBloc(RepositoryProvider.of(context))
+          ..add(InitRepositoryWithNotesEvent()),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'To-Dos App',
+          home: HomePage(),
+        ),
       ),
     );
   }
