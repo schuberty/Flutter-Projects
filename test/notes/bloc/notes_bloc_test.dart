b1f4dac8e4dc3c036dd23e0a1b04d5de7a63c013
Gabriel Schubert <schubertyi@gmail.com>
2022-03-10 09:18:25 -0300

    Added tests for NotesBloc and NoteRepository

---
+++ test/notes/bloc/notes_bloc_test.dart
@@ -0,0 +1,42 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:todo_app/models/note.dart';
+import 'package:todo_app/notes/bloc/notes_bloc.dart';
+import 'package:todo_app/repositories/notes_repository.dart';
+import 'package:hive_test/hive_test.dart';
+
+void main() {
+  group(
+    'NotesBloc',
+    () {
+      late NoteRepository noteRepository;
+      late NotesBloc notesBloc;
+
+      setUp(() async {
+        await setUpTestHive();
+        noteRepository = NoteRepository();
+        notesBloc = NotesBloc(noteRepository);
+      });
+
+      test('check if first state is NotesInitialState() in bloc.state.', () {
+        expect(notesBloc.state, NotesInitialState());
+      });
+
+      test('check if state props from NotesInitialState() is empty.', () {
+        expect(notesBloc.state.props, []);
+      });
+
+      blocTest<NotesBloc, NotesState>(
+        'emits InitRepositoryWithNotesEvent() to initialize repository and check if it\'s notes Box is empty.',
+        build: () => notesBloc,
+        act: (_) => notesBloc.add(InitRepositoryWithNotesEvent()),
+        expect: () => [],
+      );
+
+      // TODO: Implement check if Note model copyWith is equal to copied Note.
+
+      // TODO: Implement emit AddNoteEvent(...) and expect NotesLoadedState[...] with one and same added Note.
+      // TODO: Implement emit ToggleNoteCompletition(...) and expect NotesLoadedState[...] with Note having isCompleted checked true.
+    },
+  );
+}
