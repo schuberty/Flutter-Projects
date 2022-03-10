539ae3e6ae2d57113b8579457d2aa044f3d6f22e
Gabriel Schubert <schubertyi@gmail.com>
2022-03-10 09:18:55 -0300

    Added application kColors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// TODO: Implement color palette with help of https://huemint.com/
+
+const kBackgroundColor = Color(0xFF1B181B);
+const kAppBarBackgroundColor = Color(0xFF2B262B);
+const kPrimaryColor = Color(0xFFFFFFFB);
+const kSecondaryColor = Color(0xFFE5AD00);
+
+const kBottomNavigationBarColor = kBackgroundColor;
